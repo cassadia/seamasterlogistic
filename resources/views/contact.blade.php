e411276811434a70277bb2c8d1b31a8bed8b9592
Ken Arnett <kenarnettz@gmail.com>
2022-06-12 14:40:05 +0700

    update carakerja, contact,ourservice

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,7 +1,127 @@
 @extends('utama')
 
-@section('konten')
-    
-    <h1>ini adalah halaman contact</h1>
+@section('title')
+Contact | SEAMASTER LOGISTICSs
+@endsection
 
+
+@section('konten')
+
+<head>
+    <link rel='stylesheet' id='elementor-post-624-css' href="{!! asset('wp-content/uploads/elementor/css/post-62458c9.css?ver=1651048197') !!}" type='text/css' media='all' />
+</head>
+
+
+<main id=" content" class="site-main post-624 page type-page status-publish hentry" role="main">
+
+    <div class="page-content">
+        <div data-elementor-type="wp-page" data-elementor-id="624" class="elementor elementor-624">
+            <div class="elementor-inner">
+                <div class="elementor-section-wrap">
+                    <section class="elementor-section elementor-top-section elementor-element elementor-element-77e9005f elementor-section-boxed elementor-section-height-default elementor-section-height-default" data-id="77e9005f" data-element_type="section">
+                        <div class="elementor-container elementor-column-gap-extended">
+                            <div class="elementor-row">
+                                <div class="elementor-column elementor-col-33 elementor-top-column elementor-element elementor-element-4fb7ccd3" data-id="4fb7ccd3" data-element_type="column">
+                                    <div class="elementor-column-wrap elementor-element-populated">
+                                        <div class="elementor-widget-wrap">
+                                            <div class="elementor-element elementor-element-75727385 animated-slow elementor-invisible elementor-widget elementor-widget-heading" data-id="75727385" data-element_type="widget" data-settings="{&quot;_animation&quot;:&quot;fadeIn&quot;}" data-widget_type="heading.default">
+                                                <div class="elementor-widget-container">
+                                                    <h2 class="elementor-heading-title elementor-size-default">Hubungi Kami</h2>
+                                                </div>
+                                            </div>
+                                            <div class="elementor-element elementor-element-16a77dc3 elementor-button-align-stretch elementor-widget elementor-widget-form" data-id="16a77dc3" data-element_type="widget" data-settings="{&quot;button_width&quot;:&quot;25&quot;,&quot;step_next_label&quot;:&quot;Next&quot;,&quot;step_previous_label&quot;:&quot;Previous&quot;,&quot;step_type&quot;:&quot;number_text&quot;,&quot;step_icon_shape&quot;:&quot;circle&quot;}" data-widget_type="form.default">
+                                                <div class="elementor-widget-container">
+                                                    <form class="elementor-form" method="post" name="New Form">
+                                                        <input type="hidden" name="post_id" value="624" />
+                                                        <input type="hidden" name="form_id" value="16a77dc3" />
+                                                        <input type="hidden" name="referer_title" value="Contact | SEAMASTER LOGISTICS" />
+
+                                                        <input type="hidden" name="queried_id" value="624" />
+
+                                                        <div class="elementor-form-fields-wrapper elementor-labels-above">
+                                                            <div class="elementor-field-type-text elementor-field-group elementor-column elementor-field-group-name elementor-col-100">
+                                                                <label for="form-field-name" class="elementor-field-label">
+                                                                    Name </label>
+                                                                <input size="1" type="text" name="form_fields[name]" id="form-field-name" class="elementor-field elementor-size-sm  elementor-field-textual">
+                                                            </div>
+                                                            <div class="elementor-field-type-tel elementor-field-group elementor-column elementor-field-group-email elementor-col-100 elementor-field-required">
+                                                                <label for="form-field-email" class="elementor-field-label">
+                                                                    No Whatsapp </label>
+                                                                <input size="1" type="tel" name="form_fields[email]" id="form-field-email" class="elementor-field elementor-size-sm  elementor-field-textual" required="required" aria-required="true" pattern="[0-9()#&amp;+*-=.]+" title="Only numbers and phone characters (#, -, *, etc) are accepted.">
+
+                                                            </div>
+                                                            <div class="elementor-field-type-email elementor-field-group elementor-column elementor-field-group-email elementor-col-100 elementor-field-required">
+                                                                <label for="form-field-email" class="elementor-field-label">
+                                                                    Email </label>
+                                                                <input size="1" type="email" name="form_fields[email]" id="form-field-email" class="elementor-field elementor-size-sm  elementor-field-textual" required="required" aria-required="true">
+                                                            </div>
+                                                            <div class="elementor-field-type-textarea elementor-field-group elementor-column elementor-field-group-message elementor-col-100">
+                                                                <label for="form-field-message" class="elementor-field-label">
+                                                                    Comment or Message </label>
+                                                                <textarea class="elementor-field-textual elementor-field  elementor-size-sm" name="form_fields[message]" id="form-field-message" rows="4"></textarea>
+                                                            </div>
+                                                            <div class="elementor-field-group elementor-column elementor-field-type-submit elementor-col-25 e-form__buttons">
+                                                                <button type="submit" class="elementor-button elementor-size-sm">
+                                                                    <span>
+                                                                        <span class=" elementor-button-icon">
+                                                                        </span>
+                                                                        <span class="elementor-button-text">Kirim</span>
+                                                                    </span>
+                                                                </button>
+                                                            </div>
+                                                        </div>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="elementor-column elementor-col-33 elementor-top-column elementor-element elementor-element-656bd849 elementor-hidden-tablet elementor-hidden-mobile" data-id="656bd849" data-element_type="column">
+                                    <div class="elementor-column-wrap">
+                                        <div class="elementor-widget-wrap">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="elementor-column elementor-col-33 elementor-top-column elementor-element elementor-element-32d6bd2a" data-id="32d6bd2a" data-element_type="column">
+                                    <div class="elementor-column-wrap elementor-element-populated">
+                                        <div class="elementor-widget-wrap">
+                                            <div class="elementor-element elementor-element-6709a378 animated-slow elementor-invisible elementor-widget elementor-widget-heading" data-id="6709a378" data-element_type="widget" data-settings="{&quot;_animation&quot;:&quot;fadeIn&quot;}" data-widget_type="heading.default">
+                                                <div class="elementor-widget-container">
+                                                    <h2 class="elementor-heading-title elementor-size-default">Kantor Pusat</h2>
+                                                </div>
+                                            </div>
+                                            <div class="elementor-element elementor-element-5e4f64a8 elementor-widget elementor-widget-heading" data-id="5e4f64a8" data-element_type="widget" data-widget_type="heading.default">
+                                                <div class="elementor-widget-container">
+                                                    <h2 class="elementor-heading-title elementor-size-default">Jl. Griya Sejahtera No.1, RT.7/RW.4, Sunter Agung,<br>
+                                                        Tj. Priok, Kota Jkt Utara, Daerah Khusus Ibukota<br>
+                                                        Jakarta 14350</h2>
+                                                </div>
+                                            </div>
+                                            <div class="elementor-element elementor-element-f495204 elementor-widget elementor-widget-google_maps" data-id="f495204" data-element_type="widget" data-widget_type="google_maps.default">
+                                                <div class="elementor-widget-container">
+                                                    <div class="elementor-custom-embed">
+                                                        <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q=apartemen%20sunter%20icon&amp;t=m&amp;z=15&amp;output=embed&amp;iwloc=near" title="apartemen sunter icon" aria-label="apartemen sunter icon"></iframe>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div>
+        <div class="post-tags">
+        </div>
+    </div>
+
+    <section id="comments" class="comments-area">
+
+
+
+
+    </section><!-- .comments-area -->
+</main>
 @endsection
